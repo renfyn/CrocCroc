d5865d7a9709f2dec5ad99d396236193cd96ed9e
renfyn <chris94400@gmail.com>
2016-01-24 08:58:50 +0100

    factory module

---
+++ src/CrocCroc/Application/Http/Response.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 24/01/16
+ * Time: 08:38
+ */
+
+namespace CrocCroc\Application\Http;
+
+use Psr\Http\Message\MessageInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UriInterface;
+
+class Response implements ResponseInterface
+{
+
+
+
+}
---
+++ src/CrocCroc/Application/Http/ServerRequest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chris
+ * Date: 24/01/16
+ * Time: 08:34
+ */
+
+namespace CrocCroc\Application\Http;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+class ServerRequest implements ServerRequestInterface
+{
+
+
+
+
+}
